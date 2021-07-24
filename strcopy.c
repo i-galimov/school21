1dc3e7685b876b386b893ca18f36578be4995c63
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-07-24 18:38:42 +0300

    Task

--- strcopy.c
+++ strcopy.c
@@ -1,3 +1,14 @@
+/* Assignment name  : ft_strcpy
+Expected files   : ft_strcpy.c
+Allowed functions: 
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strcpy (man strcpy).
+
+Your function must be declared as follows:
+
+char    *ft_strcpy(char *s1, char *s2); */
+
 #include <stdio.h>
 #include <string.h>
 
