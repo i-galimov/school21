578b6debe9fe08ed893781a8392b9689cccf0691
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-09-25 15:05:01 +0300

    Update ft_print_alphabet.c

--- Piscine_Remote/ex_06/ft_print_alphabet.c
+++ Piscine_Remote/ex_06/ft_print_alphabet.c
@@ -1,16 +1,22 @@
 # include <unistd.h>
 
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
 void ft_print_alphabet(void) 
 {
 	char i;
+        char n;
 	
 	i = 97;
+        n = 10;
 	while (i <= 122) 
 	{
-		write(1, &i, 1);
-		i++;
+		ft_putchar(i++);
 	}
-	write(1, "\n", 1);
+	ft_putchar(n);
 }
 
 int main(void) 
