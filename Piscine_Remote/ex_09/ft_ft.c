efd1f86639f164dba1930c93dd2682c3da9e183b
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-09-25 15:52:46 +0300

    Update ft_ft.c

--- Piscine_Remote/ex_09/ft_ft.c
+++ Piscine_Remote/ex_09/ft_ft.c
@@ -1,5 +1,4 @@
-// Create a function that takes a pointer to int as a parameter, and sets the value "42"
-to that int.
+// Create a function that takes a pointer to int as a parameter, and sets the value "42" to that int.
 
 void ft_ft(int *nbr)
 {
