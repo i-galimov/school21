ae1b0e78a284512e96256716b06bce3edde2ce73
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-09-26 18:17:37 +0300

    Update ft_count_if.c

--- Piscine_Remote/ex26?/ft_count_if.c
+++ Piscine_Remote/ex26?/ft_count_if.c
@@ -1,8 +1,7 @@
 /* Create a function ft_count_if which will return the number of elements of the
 array that return 1, passed to the function f. */
 
-int
-ft_count_if(char **tab, int(*f)(char*))
+int ft_count_if(char **tab, int(*f)(char*))
 {
 	int count;
 	int i;
