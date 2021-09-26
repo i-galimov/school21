47af2f50e6301762e3e76f5b0f08a5b6bd87085e
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-09-26 03:12:27 +0300

    Update ft_strcmp.c

--- Piscine_Remote/ex17/ft_strcmp.c
+++ Piscine_Remote/ex17/ft_strcmp.c
@@ -21,7 +21,7 @@ int ft_strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s1[i] == s2[i])
 	  i++;
 	if (s1[i] > s2[i])
-		return (1);
+	  return (1);
 	else if (s1[i] < s2[i])
 	  return (-1);
 	else
