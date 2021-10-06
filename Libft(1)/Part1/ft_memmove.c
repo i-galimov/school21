69c344af44f8becea978266a73332b9545a8fe64
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-06 12:11:09 +0300

    Create ft_memmove.c

---
+++ Libft(1)/Part1/ft_memmove.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	char * src_p;
+	char * dst_p;
+    
+    if (!dst)
+		return (NULL);
+	src_p = (char*)src;
+	dst_p = (char*)dst;
+    if (dst_p < src_p)
+    {
+       while (n--)
+           *src_p++ = dst_p++;
+    }
+    else if (dst_p > src_p)
+    {
+        while (n--)
+            src_p[n-1] = dst_p[n-1];
+    }
+    return (dest);
+}
