3eed4013b76ce6e08383bba15d180a71424be012
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-06 12:30:57 +0300

    Create ft_strlcpy.c

---
+++ Libft(1)/Part1/ft_strlcpy.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	count;
+	size_t	i;
+
+	i = 0;
+	count = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	if (!dst || !src)
+		return (0);
+	while (src[count] != '\0')
+		count++;
+	while (src[i] != '\0' && i < dstsize - 1)
+		dst[i++] = src[i++];
+	dst[i] = '\0';
+	return (count);
+}
