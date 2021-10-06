10f0dcc31da1d2186edc5a0d3bccbbc17fdb1a20
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-06 07:52:19 +0300

    Create ft_memset.c

---
+++ Libft(1)/Part1/ft_memset.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+void		*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+
+	str = (unsigned char*)b;
+	while (*str)
+		*str++ = (unsigned char)c;
+	return (b);
+}
