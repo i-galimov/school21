0ef7ef7f95936df49f70bf65634b29265fc7dfc4
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-07 13:08:46 +0300

    Create ft_calloc.c

---
+++ Libft(1)/Part1/ft_calloc.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+void
+	*ft_calloc(size_t count, size_t size)
+{
+	void	*str;
+
+	str = (void*)malloc(count * size);
+	if (!str)
+		return (NULL);
+	while (count--)
+		*str++ = 0;
+	return (str);
+}
