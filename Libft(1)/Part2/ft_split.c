64f4aaee1aea086a8ea731bdcf908276a9c66fc4
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-09 01:33:06 +0300

    Update ft_split.c

--- Libft(1)/Part2/ft_split.c
+++ Libft(1)/Part2/ft_split.c
@@ -1,1 +1,60 @@
-char **ft_split(char const *s, char c);
+static int ft_count_words(const char *str, char c)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (*str)
+	{
+		if (*str != c && count == 0)
+		{
+			count = 1;
+			i++;
+		}
+		else if (*str == c)
+			count = 0;
+		str++;
+	}
+	return (i);
+}
+
+static char * ft_word_dup(const char *str, int start, int end)
+{
+	char	*word;
+	int	i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	while (start < end)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int	index;
+	char	**split;
+
+	if (!s || !(split = malloc((count_words(s, c) + 1) * sizeof(char *))))
+		return (0);
+	i = 0;
+	j = 0;
+	i_start = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && i_start < 0)
+			i_start = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && i_start >= 0)
+		{
+			split[j++] = ft_word_dup(s, i_start, i);
+			i_start = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
+}
