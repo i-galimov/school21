d32ec54b90cc608a75dd081501c6cd4c07bd37a9
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-09 01:25:31 +0300

    Update libft.h

--- Libft(1)/libft.h
+++ Libft(1)/libft.h
@@ -35,8 +35,7 @@ void ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void ft_putstr_fd(char *s, int fd);
 char **ft_split(char const *s, char c);
-void ft_striteri(char *s, void (*f)(unsigned int,
-char*));
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const s2);
 char *ft_strmapi(char const *s, char (f)(unsigned int, char));
 char *ft_strtrim(char const *s1, char const *set);
