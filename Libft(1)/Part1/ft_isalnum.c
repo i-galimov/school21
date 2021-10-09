54f8128d0d221fdbba263f147b71f56461a86cf5
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-09 14:19:34 +0300

    Update ft_isalnum.c

--- Libft(1)/Part1/ft_isalnum.c
+++ Libft(1)/Part1/ft_isalnum.c
@@ -1,6 +1,6 @@
 int ft_isalnum (int ch)
 {
-	if (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+	if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 	{
 		return (ch);
 	}
