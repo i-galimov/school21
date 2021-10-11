3f686751deb5c71a105478b5e713eeffc422086b
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-11 20:21:54 +0300

    Create ft_lstadd_back.c

---
+++ Libft(1)/Bonus/ft_lstadd_back.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*list;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		list = *lst;
+		while (list->next != NULL)
+			list = list->next;
+		list->next = new;
+	}
+}
